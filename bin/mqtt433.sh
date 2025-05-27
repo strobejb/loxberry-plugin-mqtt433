0844f1e8cedab2a7a37524f3415c76a27846d329
James Brown <james@catch22.net>
2025-05-27 08:25:20 +0100

    bash wrapper

---
+++ bin/mqtt433.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ENVIRONMENT=$(cat /etc/environment)
+export $ENVIRONMENT
+
+REPLACELBPBINDIR/venv/bin/python3 REPLACELBPBINDIR/mqtt433.py --logfile=REPLACELBPLOGDIR/mqtt433.log --configfile=REPLACELBPCONFIGDIR/mqtt433.cfg
+
